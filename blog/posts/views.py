59dd79f1c250d93d8845db184a1557af3e86ca95
kingkonggrunt <little-kk@hotmail.com>
2022-09-16 02:12:05 +1000

    feat: final commit

---
+++ blog/posts/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+def index(request):
+    
+    context = {
+        'posts': Post.objects.all()
+    }
+    
+    return render(request, 'index.html', context)
+
+def post(request, pk):
+    post = Post.objects.get(id=pk)
+    
+    context = {
+        'post': post
+    }
+    
+    return render(request, 'post.html', context)
